1742bd07f298ccf90c63258ffd9d9f09e7d0dd2c
rache <rache@DEEP_BLUE>
2025-09-21 16:24:33 -0300

    implementação de mais dados na exibição

--- MauiAppTempo2/MainPage.xaml.cs
+++ MauiAppTempo2/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿namespace MauiAppTempo2
+﻿using MauiAppTempo2.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempo2
 {
     public partial class MainPage : ContentPage
     {
@@ -9,16 +12,59 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                                         $"Longitude: {t.lon} \n" +
+                                         $"Nascer do Sol: {t.sunrise} \n" +
+                                         $"Por do Sol: {t.sunset} \n" +
+                                         $"Temp Max: {t.temp_max} \n" +
+                                         $"Temp Min: {t.temp_min} \n" +
+                                         $"Clima: {t.description}\n" +
+                                         $"Velocidade do Vento: {t.speed} m/s\n" +
+                                         $"Visibilidade: {t.visibility} m";
+
+
+                        lbl_res.Text = dados_previsao;
+
+
+                    }
+                    else
+                    {
+                        await DisplayAlert("Erro", "Cidade não encontrada. Verifique o nome e tente novamente.", "OK");
+                        lbl_res.Text = "";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a cidade.";
+
+
+                }
+            }
+            catch (HttpRequestException)
+            {
+                await DisplayAlert("Erro", "Sem conexão com a internet.", "OK");
+
+            } catch(Exception ex) 
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+            }
+
+
+
+
         }
     }
 
